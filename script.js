f5b1a7ab38f15fd16ce5ac32046441e966697097
Richard Cole Tumolac <rctumolac@gmail.com>
2017-01-18 23:24:58 +0000

    Changed the CSS and the JavaSript to make an Italian Flag

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
 //Write your function calls below this line
+setColor(0, 0, "Green");
+setColor(1, 0, "Green");
+setColor(2, 0, "Green");
+setColor(3, 0, "Green");
+setColor(4, 0, "Green");
+setColor(5, 0, "Green");
+setColor(6, 0, "Green");
+setColor(7, 0, "Green");
+setColor(8, 0, "Green");
+setColor(9, 0, "Green");
+
+setColor(0, 1, "Green");
+setColor(1, 1, "Green");
+setColor(2, 1, "Green");
+setColor(3, 1, "Green");
+setColor(4, 1, "Green");
+setColor(5, 1, "Green");
+setColor(6, 1, "Green");
+setColor(7, 1, "Green");
+setColor(8, 1, "Green");
+setColor(9, 1, "Green");
+
+setColor(0, 2, "Green");
+setColor(1, 2, "Green");
+setColor(2, 2, "Green");
+setColor(3, 2, "Green");
+setColor(4, 2, "Green");
+setColor(5, 2, "Green");
+setColor(6, 2, "Green");
+setColor(7, 2, "Green");
+setColor(8, 2, "Green");
+setColor(9, 2, "Green");
+
+setColor(0, 7, "Red");
+setColor(1, 7, "Red");
+setColor(2, 7, "Red");
+setColor(3, 7, "Red");
+setColor(4, 7, "Red");
+setColor(5, 7, "Red");
+setColor(6, 7, "Red");
+setColor(7, 7, "Red");
+setColor(8, 7, "Red");
+setColor(9, 7, "Red");
+
+setColor(0, 8, "Red");
+setColor(1, 8, "Red");
+setColor(2, 8, "Red");
+setColor(3, 8, "Red");
+setColor(4, 8, "Red");
+setColor(5, 8, "Red");
+setColor(6, 8, "Red");
+setColor(7, 8, "Red");
+setColor(8, 8, "Red");
+setColor(9, 8, "Red");
+
+setColor(0, 9, "Red");
+setColor(1, 9, "Red");
+setColor(2, 9, "Red");
+setColor(3, 9, "Red");
+setColor(4, 9, "Red");
+setColor(5, 9, "Red");
+setColor(6, 9, "Red");
+setColor(7, 9, "Red");
+setColor(8, 9, "Red");
+setColor(9, 9, "Red");
